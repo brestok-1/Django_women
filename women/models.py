ffec12a7f98ea08aa8c7b9cd215243d2410f1cc7
brestok-1 <6234513@gmail.com>
2022-12-08 10:36:42 +0300

    women_tags

--- women/models.py
+++ women/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.urls import reverse
+
 
 # Create your models here.
+class Women(models.Model):
+    title = models.CharField(max_length=250, verbose_name='Title')
+    content = models.TextField(blank=True, verbose_name="Article's content")
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Category')
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post', kwargs={'post_id': self.pk})
+
+    class Meta:
+        verbose_name = verbose_name_plural = 'Famous Women'
+        ordering = ['-time_create', 'title']
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True, verbose_name='Category')
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'cat_id': self.pk})
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = "Categorie's"
+        ordering = ['id']
--- women/views.py
+++ women/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseNotFound, Http404
+from .models import *
 
-# Create your views here.
+menu = [{'title': 'About website', 'url_name': 'about'},
+        {'title': 'Add article', 'url_name': 'addpage'},
+        {'title': 'Feedback', 'url_name': 'contact'},
+        {'title': 'Sign up', 'url_name': 'login'}]
+
+
+def index(request):
+    posts = Women.objects.all()
+    context = {'title': 'Main Page',
+               'menu': menu,
+               'posts': posts,
+               'cat_selected': 0,
+               }
+
+    return render(request, 'women/index.html', context=context)
+
+
+def about(request):
+    return render(request, 'women/about.html', {'title': 'About Web page', 'menu': menu})
+
+
+def addpage(request):
+    return HttpResponse('add article')
+
+
+def contact(request):
+    return HttpResponse('feedback')
+
+
+def login(request):
+    return HttpResponse('auth')
+
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound("<h1>The web-page not found</h1>")
+
+
+def show_post(request, post_id):
+    return HttpResponse(f"Show the article with id = {post_id}")
+
+
+def show_category(request, cat_id):
+    posts = Women.objects.filter(cat_id=cat_id)
+    context = {'title': 'Display by category',
+               'menu': menu,
+               'posts': posts,
+               'cat_selected': cat_id,
+               }
+    if len(posts) == 0:
+        raise Http404()
+    return render(request, 'women/index.html', context=context)
