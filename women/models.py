df110fed9a2d58e04fe375f6e1f786b2111a515e
brestok-1 <6234513@gmail.com>
2022-12-27 11:25:48 +0300

    afer cache

--- women/models.py
+++ women/models.py
@@ -36,7 +36,7 @@ class Category(models.Model):
 
     class Meta:
         verbose_name = 'Category'
-        verbose_name_plural = "Categorie's"
+        verbose_name_plural = "Categories"
         ordering = ['id']
 
 
